0e1432e8ed816a07d4870c85066f869bce7fe143
Consultadd Services <consultadd@Consultadds-MacBook-Pro.local>
2025-01-09 12:26:23 +0530

    args and kwardgs added

--- python_3/agrgs&kwargs.py
+++ python_3/agrgs&kwargs.py
@@ -13,5 +13,40 @@ def sum_all(*args):
 print(sum_all(1,2,3,4))
 print(sum_all(1,2,3,4,5,6,7,8))
 
+def display_info(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
 
+display_info(name="Ansh", age=25, city="Mumbai")
+
+
+def greet(*args):
+    for name in args:
+        print(f"Hello, {name}!")
+
+greet("Ansh", "Divyansh", "Ayush")  
+# Output:
+# Hello, Ansh!
+# Hello, Divyansh!
+# Hello, Ayush!
+
+# keywords argyumensts kwargs
+
+def intro(**kwargs):
+    for key,value in kwargs.items():
+        print(f"{key}:{value}")
+
+intro(name="ansh",age=21,intrests="storytelling")
+
+
+
+def fun(*args, **kwargs):
+    print("Positional arguments:", args)
+    print("Keyword arguments:", kwargs)
+
+fun(1, 2, 3, a=4, b=5)
+
+def intro_more(*args,**kwargs):
+    for key,value in kwargs.items():
+        print(f"{key}:{value}")
 
